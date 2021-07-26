46bf1130c0d9237cb942e1eff160a2c7bac2661c
Wei Kin Huang <weikin.huang04@gmail.com>
2021-07-26 16:06:28 -0400

    Fix for https reqests through request relay

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,11 +1,13 @@
 import * as http from 'http';
+import * as https from 'https';
 import * as url from 'url';
 import { SocksProxyAgent } from 'socks-proxy-agent';
 import { DIRECT_PROXY_MODE, getProxy } from './util';
 import logger from './logger';
 
 function _request(uri: url.URL, req: http.IncomingMessage, res: http.ServerResponse, agent?: http.Agent | null): void {
-  const proxy = http.request({
+  const httpModule = uri.protocol === 'https:' ? https : http;
+  const proxy = httpModule.request({
     port: uri.port,
     hostname: uri.hostname,
     path: uri.pathname + uri.search,
@@ -15,12 +17,26 @@ function _request(uri: url.URL, req: http.IncomingMessage, res: http.ServerRespo
   });
 
   req.on('error', (e) => {
-    logger.error({ channel: 'request', message: e.message, stack: e.stack });
+    logger.error({
+      channel: 'request',
+      message: e.message,
+      hostname: uri.hostname,
+      port: uri.port,
+      protocol: uri.protocol,
+      stack: e.stack,
+    });
     proxy.destroy(e);
   });
 
   proxy.on('error', (e) => {
-    logger.error({ channel: 'request', message: e.message, stack: e.stack });
+    logger.error({
+      channel: 'request',
+      message: e.message,
+      hostname: uri.hostname,
+      port: uri.port,
+      protocol: uri.protocol,
+      stack: e.stack,
+    });
     res.writeHead(500);
     res.end('Connection error\n');
   });
@@ -35,29 +51,40 @@ function _request(uri: url.URL, req: http.IncomingMessage, res: http.ServerRespo
 
 export async function request(req: http.IncomingMessage, res: http.ServerResponse): Promise<void> {
   const uri = new url.URL(req.url);
-  const proxy = await getProxy(`http://${uri.hostname}`, uri.hostname.split(':')[0]);
-
-  logger.debug({
-    channel: 'request',
-    message: 'request received',
-    host: uri.hostname,
-    proxy: proxy !== DIRECT_PROXY_MODE ? `${proxy.host}:${proxy.port}` : '',
-  });
 
   try {
-    // conditionally forward through socks proxy
-    if (proxy !== DIRECT_PROXY_MODE) {
-      return _request(uri, req, res, new SocksProxyAgent(proxy));
-    } else {
-      return _request(uri, req, res);
+    const proxy = await getProxy(`http://${uri.hostname}`, uri.hostname.split(':')[0]);
+
+    logger.debug({
+      channel: 'request',
+      message: 'request received',
+      host: uri.hostname,
+      proxy: proxy !== DIRECT_PROXY_MODE ? `${proxy.host}:${proxy.port}` : '',
+    });
+
+    try {
+      // conditionally forward through socks proxy
+      if (proxy !== DIRECT_PROXY_MODE) {
+        return _request(uri, req, res, new SocksProxyAgent(proxy));
+      } else {
+        return _request(uri, req, res);
+      }
+    } catch (e) {
+      logger.error({
+        channel: 'request',
+        message: e.message,
+        host: uri.hostname,
+        proxy: proxy !== DIRECT_PROXY_MODE ? `${proxy.host}:${proxy.port}` : '',
+        stack: e.stack,
+      });
+      try {
+        res.writeHead(500);
+        res.end('Connection error\n');
+      } catch {
+        // empty
+      }
     }
   } catch (e) {
-    logger.error({ channel: 'request', message: e.message, stack: e.stack });
-    try {
-      res.writeHead(500);
-      res.end('Connection error\n');
-    } catch {
-      // empty
-    }
+    logger.error({ channel: 'request', message: e.message, host: uri.hostname, stack: e.stack });
   }
 }
